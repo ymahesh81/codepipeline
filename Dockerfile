49ffbe80b1112d6471a3f46a2fe8480f70acd387
Joseph Assouramou <jo@Josephs-MacBook-Pro.local>
2022-06-14 18:23:42 -0400

    test

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM REPOSITORY_URI:latest
+FROM REPOSITORY_URI/demo-nginx:latest
 ADD index.html /usr/share/nginx/html/
 EXPOSE 80
--- nginx-test-ecr.sh
+++ nginx-test-ecr.sh
@@ -2,7 +2,7 @@
 ACCOUNT_ID="886774924794"
 AWS_DEFAULT_REGION="us-east-1"
 REPOSITORY_URI="$ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
-IMAGE_REPO_NAME="demo"
+IMAGE_REPO_NAME="demo-nginx"
 docker pull nginx:latest
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $REPOSITORY_URI 
 docker tag nginx:latest $REPOSITORY_URI/$IMAGE_REPO_NAME:latest
